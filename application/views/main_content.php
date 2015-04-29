76955bd12a8428c090da96792d2fc37949e23fdc
ozkula <at.ozkula@gmail.com>
2015-04-29 10:32:39 +0700

    FIX img Responsive

--- application/views/main_content.php
+++ application/views/main_content.php
@@ -4,24 +4,26 @@
 		Maecenas ac fringilla dui. Mauris eu venenatis nisi. Etiam eget ipsum viverra, consequat neque sit amet, imperdiet eros. Etiam tristique hendrerit nisl, ut pellentesque nisl iaculis in. Duis pretium, velit eleifend posuere consequat, tortor nisl dictum enim, at ultrices augue sem ornare ex. Suspendisse semper, nisl quis luctus aliquet, quam erat varius neque, eu varius dolor felis ac tortor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed ac justo suscipit, suscipit turpis vel, posuere nisi. Donec et molestie mi.
 		Nunc ac neque ultricies, hendrerit sem ac, posuere turpis. Sed ut nisi tortor. In eu dui lorem. Nullam ultrices diam volutpat suscipit venenatis. Cras nisl eros, fermentum eget risus sit amet, dignissim fermentum lectus. Duis eget tempor leo. Cras aliquet est non orci dictum congue. Duis euismod finibus diam, eu mollis neque egestas non. Aliquam viverra scelerisque sem tempor tristique. Nunc nec porta arcu. Cras id dolor finibus, consequat ipsum sed, aliquet dui. Vivamus nec justo nibh. Mauris varius odio placerat accumsan dignissim. Sed tempus aliquam sem et iaculis. </p>
 	</div>
-	<div class="col-md-4"><img src="http://placehold.it/350x350"></div>
+	<div class="col-md-4"><img src="http://placehold.it/350x350" class="img-responsive"></div>
 </div>
 <br>
 <div class="row">
 	<div class="col-md-6">
 		<div class="thumbnail">
-	      <img src="http://placehold.it/150x150" alt="Family Photo" class="img-circle">
+	      <a href="picture"><img src="http://placehold.it/150x150" alt="Family Photo" class="img-circle">
 	      <div class="caption">
-	      	<h3 align="center"><a href="picture">Familly Photo</a></h3>
+	      	<h3 align="center">Familly Photo</h3>
 	      </div>
+	      </a>
 	  	</div>
 	</div>
 	<div class="col-md-6">
 		<div class="thumbnail">
-	      <img src="http://placehold.it/150x150" alt="Family Photo" class="img-circle">
+	      <a href="about"><img src="http://placehold.it/150x150" alt="Family Photo" class="img-circle">
 	      <div class="caption">
-	        <h3 align="center"><a href="about">About This Family</a></h3>
+	        <h3 align="center">About This Family</h3>
 	      </div>
+	      </a>
 	  	</div>
 	</div>
 </div>
